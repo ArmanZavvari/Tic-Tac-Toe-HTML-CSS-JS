1250c3adac7ae0e4111314b61293049a90524d30
Arman Zavvari <122162776+ArmanZavvari@users.noreply.github.com>
2023-09-03 23:01:31 +0330

    Update script.js
    Restart Added to game

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let isXTurn = true;
 
 cells.forEach((cell) => {
   cell.addEventListener("click", () => {
+    if (cell.textContent === "") {
       if (isXTurn) {
         cell.textContent = "X";
         status.textContent = "It's O's turn";
@@ -14,6 +15,18 @@ cells.forEach((cell) => {
         status.textContent = "It's X's turn";
       }
       isXTurn = !isXTurn;
-
+    }
   });
 });
+const restart = document.querySelector(".game--restart");
+restart.addEventListener("click", () => {
+  const cells = document.querySelectorAll(".cell");
+
+  cells.forEach((cell) => {
+    cell.textContent = ""; // Clear the cell content
+  });
+
+  status.textContent = "It's X's turn"; // Reset status message
+  isXTurn = true; // Reset the turn to X's
+});
+
